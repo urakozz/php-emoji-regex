9e0ba96af8ecbb0917eba9b99d36d0cff2c5bdb6
urakozz <urakozz@gmail.com>
2015-05-20 14:37:58 +0200

    tests

--- tests/InitTest.php
+++ tests/InitTest.php
@@ -17,12 +17,77 @@ use Kozz\Components\Emoji\EmojiParser;
 class InitTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testLoad()
+    public function testMatchAll()
     {
+        // codes are here:
+        // http://apps.timwhitlock.info/emoji/tables/unicode#block-1-emoticons
+
         $text = "a #💩 #and #🍦 #😳";
         $parser = new EmojiParser();
         $parser->setPrepend("#\\w+|#");
-        $pattern = $parser->matchAll($text);
-        var_dump($pattern);
+        $matches = $parser->matchAll($text);
+
+        $this->assertNotEmpty($matches);
+        $this->assertNotEmpty($matches[0]);
+        $this->assertEquals(4, count($matches[0]));
+
+        $this->assertGreaterThan(0, preg_match("/\\x{1F4A9}/u", $matches[0][0]));
+        $this->assertGreaterThan(0, preg_match("/^\#\\x{1F4A9}$/u", $matches[0][0]));
+        $this->assertGreaterThan(0, preg_match("/\xF0\x9F\x92\xA9/", $matches[0][0]));
+        $this->assertGreaterThan(0, preg_match("/^\#\xF0\x9F\x92\xA9$/", $matches[0][0]));
+
+        $this->assertGreaterThan(0, preg_match("/^#and$/u", $matches[0][1]));
+
+        $this->assertGreaterThan(0, preg_match("/\\x{1F366}/u", $matches[0][2]));
+        $this->assertGreaterThan(0, preg_match("/^\#\\x{1F366}$/u", $matches[0][2]));
+        $this->assertGreaterThan(0, preg_match("/\xF0\x9F\x8D\xA6/", $matches[0][2]));
+        $this->assertGreaterThan(0, preg_match("/^\#\xF0\x9F\x8D\xA6$/", $matches[0][2]));
+
+        $this->assertGreaterThan(0, preg_match("/\\x{1F633}/u", $matches[0][3]));
+        $this->assertGreaterThan(0, preg_match("/^\#\\x{1F633}$/u", $matches[0][3]));
+        $this->assertGreaterThan(0, preg_match("/\xF0\x9F\x98\xB3/", $matches[0][3]));
+        $this->assertGreaterThan(0, preg_match("/^\#\xF0\x9F\x98\xB3$/", $matches[0][3]));
+    }
+
+    public function testPattern()
+    {
+        $parser = new EmojiParser();
+        $pattern = $parser->getPattern(null);
+
+        $parser->setPrepend("#");
+        $this->assertEquals("/#".$pattern."/u", $parser->getPattern());
+
+    }
+
+    public function testReplaceCallback()
+    {
+        $parser = new EmojiParser();
+        $parser->setPrepend("#\\w+|#");
+        $text = "a #💩 #and or #🍦 #😳";
+        $i = 0;
+        $matches = [];
+
+        $replaced = $parser->replaceCallback($text, function($match)use(&$i, &$matches){
+            $key = '_$'.$i++.'_';
+            $matches[$key] = $match[0];
+            return $key;
+        });
+
+        $this->assertEquals("a _$0_ _$1_ or _$2_ _$3_", $replaced);
+    }
+
+    public function testReplace()
+    {
+        $text = "a #💩 #and or #🍦 #😳";
+        $parser = new EmojiParser();
+        $replaced = $parser->replace($text, "q");
+        $this->assertEquals("a #q #and or #q #q", $replaced);
+    }
+
+    public function testMatch(){
+        $text = "a #💩 #and or #🍦 #😳";
+        $parser = new EmojiParser();
+        $match = $parser->match($text);
+        $this->assertGreaterThan(0, $match);
     }
 }
